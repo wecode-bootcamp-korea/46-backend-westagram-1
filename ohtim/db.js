e6bc61a3664b840a693c2ef0808b150957bfa4df
ts-oh <this.tsoh@gmail.com>
2023-05-17 17:53:54 +0900

    :fire: [REMOVE]: removed unused db variables

--- ohtim/db.js
+++ ohtim/db.js
@@ -2,13 +2,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import { DataSource } from 'typeorm'
 
-const db_host = process.env.DB_HOST
-const db_user = process.env.DB_USER
-const db_pass = process.env.DB_PASS
-const db_database = process.env.DB_DATABASE
-const db_type = process.env.DB_CONNECTION
-const db_port = process.env.DB_PORT
-const db_log = process.env.DB_LOGGING
 const PORT = process.env.PORT
 
 const myDataSource = new DataSource({
