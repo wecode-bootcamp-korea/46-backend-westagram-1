8a0c717522860fdc759408ab5587cf239d167c0c
김상우 <gimsang-u@Leeui-MacBook-Pro.local>
2023-05-17 11:21:49 +0900

    assignment 2

---
+++ sangwoo/app.js
@@ -0,0 +1,53 @@
+require("dotenv").config(); //환경변수
+
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan"); // morgan 모듈 추가하기
+const app = express();
+const { DataSource } = require("typeorm");
+
+//미들웨어
+app.use(cors());
+app.use(logger("combined"));
+app.use(express.json());
+
+const port = process.env.PORT;
+
+const appDataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+appDataSource.initialize().then(() => {
+  console.log("Data Source has been initialized!");
+});
+
+app.get("/ping", function (req, res, next) {
+  res.json({ message: "pong" });
+});
+
+//create a users
+
+app.post("/users", async (req, res) => {
+  const { name, email, profileImage, password } = req.body; //구조분해할당
+
+  await appDataSource.query(
+    `INSERT INTO users(
+              name,
+              email,
+              profile_image,
+              password
+          ) VALUES (?, ?, ?, ?);
+          `,
+    [name, email, profileImage, password]
+  );
+  res.status(201).json({ message: "usersCreated" });
+});
+
+app.listen(8000, function () {
+  console.log("server listening on port 8000");
+});
