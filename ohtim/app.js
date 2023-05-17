fb51fe0a900a331b6056623ba13eb7391a7b8e70
ts-oh <this.tsoh@gmail.com>
2023-05-17 17:21:09 +0900

    :bug: [FIX]: Add missing import to create new post

--- ohtim/app.js
+++ ohtim/app.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
-import { newUserSignUp } from './apps/users/usersAPI.js'
 import { PORT } from './db.js'
 import { myDataSource } from './db.js'
+import { newUserSignUp } from './apps/users/usersAPI.js'
+import { userCreateNewPost } from './apps/posts/newpostAPI.js'
 
 const app = express()
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.post('/users/signup', newUserSignUp)
+app.post('/posts/post', userCreateNewPost)
 myDataSource.initialize()
 
 app.listen(PORT, () => {
