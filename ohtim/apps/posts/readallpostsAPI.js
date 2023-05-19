d7d13bc5ec4a494c89af7251146f584bd9560c23
ts-oh <this.tsoh@gmail.com>
2023-05-19 10:36:21 +0900

    :bug: [FIX]: sql query to join users.id with posts.user_id

--- ohtim/apps/posts/readallpostsAPI.js
+++ ohtim/apps/posts/readallpostsAPI.js
@@ -4,9 +4,14 @@ import { app } from '../../app.js'
 const readAllPosts = async (req, res) => {
   try {
     const queryData = await myDataSource.query(
-      `SELECT posts.user_id AS userId, users.profile_image AS userProfileImage, 
-        posts.id AS postingId, posts.image_url AS postingImageUrl, posts.content AS postingContent 
-        FROM posts, users`
+      `SELECT 
+          posts.user_id AS userId, 
+          users.profile_image AS userProfileImage, 
+          posts.id AS postingId, 
+          posts.image_url AS postingImageUrl, 
+          posts.content AS postingContent 
+        FROM posts
+        JOIN users ON users.id = posts.user_id`
     )
     console.log(queryData)
     res.status(201).json({ data: queryData })
